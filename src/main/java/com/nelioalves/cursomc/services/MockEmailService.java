b6e662d318bdcd64ed8aa67c54c0e6ed6d2ef705
Marcos Pereira <mpdsmarcos@gmail.com>
2018-06-26 01:13:17 -0300

    MockEmailService com Logger. Padroes Strategy e Template Method

--- src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
@@ -10,6 +10,7 @@ public class MockEmailService extends AbstractEmailService {
 	
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
+		
 		LOG.info("Simulando envio de email...");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");	
